66d8e8e03cd48b69e1245c689fc5dc9cd2fd0fc9
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2022-09-12 20:37:32 +0200

    prepare release action

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,111 @@
 module.exports = function(grunt) {
 
-  require('load-grunt-tasks')(grunt);
+	require( 'load-grunt-tasks' )( grunt );
 
-  // Project configuration.
-  grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
+	// Project configuration.
+	grunt.initConfig(
+        {
+		pkg: grunt.file.readJSON( 'package.json' ),
 
-	// # Internationalization
+		// # Build and release
 
-	// Add text domain
-	addtextdomain: {
-		options: {
-            textdomain: '<%= pkg.name %>',    // Project text domain.
-            updateDomains: [ 'wc-mnm-checkboxes' ]  // List of text domains to replace.
-        },
-		target: {
-			files: {
-				src: ['*.php', '**/*.php', '**/**/*.php', '!node_modules/**', '!deploy/**']
+		// Remove any files in zip destination and build folder
+		clean: {
+			main: ['build/**']
+		},
+
+		// Copy the plugin into the build directory
+		copy: {
+			main: {
+				src: [
+				'**',
+				'!node_modules/**',
+				'!build/**',
+				'!deploy/**',
+				'!svn/**',
+				'!**/*.zip',
+				'!**/*.bak',
+				'!wp-assets/**',
+				'!package-lock.json',
+				'!nyp-logo.png',
+				'!screenshots/**',
+				'!.git/**',
+				'!**.md',
+				'!Gruntfile.js',
+				'!package.json',
+				'!gitcreds.json',
+				'!.gitcreds',
+				'!.gitignore',
+				'!.gitmodules',
+				'!sftp-config.json',
+				'!**.sublime-workspace',
+				'!**.sublime-project',
+				'!deploy.sh',
+				'!**/*~',
+				'!phpcs.xml',
+				'!composer.json',
+				'!composer.lock',
+				'!vendor/**',
+				'!none',
+				'!includes/compatibility/backcompatibility/**'
+				],
+				dest: 'build/'
 			}
-		}
-	},
+		},
 
-	// Generate .pot file
-	makepot: {
-		target: {
+		// Make a zipfile.
+		compress: {
+			main: {
+				options: {
+					mode: 'zip',
+					archive: 'deploy/<%= pkg.version %>/<%= pkg.name %>.zip'
+				},
+				expand: true,
+				cwd: 'build/',
+				src: ['**/*'],
+				dest: '/<%= pkg.name %>'
+			}
+		},
+
+		// # Internationalization
+
+		// Add text domain
+		addtextdomain: {
 			options: {
-				domainPath: '/languages', // Where to save the POT file.
-				exclude: ['deploy'], // List of files or directories to ignore.
-				mainFile: 'wc-mnm-checkboxes.php', // Main project file.
-				potFilename: 'wc-mnm-checkboxes.pot', // Name of the POT file.
-				type: 'wp-plugin', // Type of project (wp-plugin or wp-theme).
-				potHeaders: {
-                    'Report-Msgid-Bugs-To': 'https://github.com/kathyisawesome/wc-mnm-checkboxes/issues/'
-                } 
+				textdomain: '<%= pkg.name %>'    // Project text domain.
+			},
+			target: {
+				files: {
+					src: ['*.php', '**/*.php', '**/**/*.php', '!node_modules/**', '!deploy/**']
+				}
 			}
-		}
-	},
+		},
 
+		// Generate .pot file
+		makepot: {
+			target: {
+				options: {
+					domainPath: '/languages', // Where to save the POT file.
+					exclude: ['deploy','build','node_modules'], // List of files or directories to ignore.
+					mainFile: '<%= pkg.name %>.php', // Main project file.
+					potFilename: '<%= pkg.name %>.pot', // Name of the POT file.
+					type: 'wp-plugin', // Type of project (wp-plugin or wp-theme).
+					potHeaders: {
+						'Report-Msgid-Bugs-To': '<%= pkg.bugs.issues %>'
+					}
+				}
+			}
+		},
 
-	// bump version numbers (replace with version in package.json)
-	replace: {
-		Version: {
-			src: [
+		// bump version numbers (replace with version in package.json)
+		replace: {
+			Version: {
+				src: [
 				'readme.txt',
 				'<%= pkg.name %>.php'
-			],
-			overwrite: true,
-			replacements: [
+				],
+				overwrite: true,
+				replacements: [
 				{
 					from: /Stable tag:.*$/m,
 					to: "Stable tag: <%= pkg.version %>"
@@ -60,13 +119,34 @@ module.exports = function(grunt) {
 					to: "public $version = '<%= pkg.version %>'"
 				},
 				{
-					from: /public \$version      = \'.*.'/m,
-					to: "public $version      = '<%= pkg.version %>'"
+					from: /public \$version = \'.*.'/m,
+					to: "public $version = '<%= pkg.version %>'"
+				},
+				{
+					from: /public static \$version = \'.*.'/m,
+					to: "public static $version = '<%= pkg.version %>'"
+				},
+				{
+					from: /const VERSION = \'.*.'/m,
+					to: "const VERSION = '<%= pkg.version %>'"
 				}
-			]
+				]
+			}
 		}
-	}
 
-  });
+        }
+    );
+
+	grunt.registerTask(
+        'zip',
+        [
+		'clean',
+		'copy',
+		'compress'
+        ]
+    );
+
+	grunt.registerTask( 'build', [ 'replace', 'addtextdomain', 'makepot' ] );
+	grunt.registerTask( 'release', [ 'build', 'zip', 'clean' ] );
 
 };
